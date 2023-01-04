d46d157c2b70bb4f375cc6c25256b0c2cf1e6bc7
newRagnaros84 <ragnaros84@mail.ru>
2023-01-04 18:12:57 +0500

    new commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class Main {
+
+    @BeforeEach
+    public void initDriver() throws IOException {
+        final String url = "http://localhost:4444/wd/hub";
+        WebDriver driver = new RemoteWebDriver(new URL(url), DesiredCapabilities.chrome());
+        driver.manage().window().setSize(new Dimension(1920,1024));
+        WebDriverRunner.setWebDriver(driver);
+    }
+
+    @Test
+    @DisplayName("Открываем сщуствующую Issue")
+    public void testIssue() {
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Открываем страницу с репозиторием", () -> {
+            $x("//*[contains(@class, 'header-search-input')]").click();
+            $x("//*[contains(@class, 'header-search-input')]").sendKeys("eroshenkoam/allure-example");
+            $x("//*[contains(@class, 'header-search-input')]").submit();
+            $x("//a[@href='/eroshenkoam/allure-example']").click();
+        });
+        step("Открываем страницу с задачами в репозитории", () -> {
+            $x("//a[contains(@data-selected-links, 'repo_issues')]").click();
+            $x("//a[@aria-label='Page 2']").click();
+        });
+//        step("Проверяем title", () -> {
+//            $x("//a[@id='issue_12_link']").click();
+//            $x("//h1[contains(@class, 'gh-header-title')]").should(Condition.text("Hello, World!"));
+//        });
+    }
+
+    @AfterEach
+    public void stopDriver() {
+        Optional.ofNullable(WebDriverRunner.getWebDriver()).ifPresent(WebDriver::quit);
+    }
+
+}
